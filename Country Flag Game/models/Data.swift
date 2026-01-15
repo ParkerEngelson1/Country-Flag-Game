d5b21164b80fb9c4d64c8613900deac8f0b105a6
Parker <Parker.engelson09@gmail.com>
2026-01-15 10:31:35 -0800

    Created Data and GameManager

---
+++ Country Flag Game/models/Data.swift
@@ -0,0 +1,32 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Parker Engelson on 1/13/26.
+//
+
+import SwiftUI
+struct Data {
+    let countries = [
+        "Angola",
+        "Austria",
+        "China",
+        "England",
+        "Estonia",
+        "France",
+        "Germany",
+        "Greece",
+        "India",
+        "Israel",
+        "Italy",
+        "Norway",
+        "Pakistan",
+        "Peru",
+        "Saudi Arabia",
+        "South Africa",
+        "Spain",
+        "Sweden",
+        "Ukraine",
+        "United States"
+    ]
+}
